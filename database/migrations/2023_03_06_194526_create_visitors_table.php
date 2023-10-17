c19a73a48fb3ef56da1735b6a2f40cc1e8f1b124
Paschal Mizengo <47353288+ngoijr3107@users.noreply.github.com>
2023-10-17 15:08:40 +0300

    add rating column

--- database/migrations/2023_03_06_194526_create_visitors_table.php
+++ database/migrations/2023_03_06_194526_create_visitors_table.php
@@ -26,6 +26,7 @@ class CreateVisitorsTable extends Migration
             $table->string('status')->default('in');
             $table->dateTime('checkintime');
             $table->dateTime('checkouttime');
+            $table->string('rate');
             $table->string('comment');
             $table->timestamps();
         });
