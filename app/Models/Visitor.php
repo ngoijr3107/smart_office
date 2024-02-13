db4d008ee169e714e9b01914f732dc7b95036e79
Paschal Mizengo <47353288+ngoijr3107@users.noreply.github.com>
2024-02-13 10:14:46 +0300

    add purpose relationship

--- app/Models/Visitor.php
+++ app/Models/Visitor.php
@@ -5,17 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Department;
+use App\Models\VisitPurpose;
 
 class Visitor extends Model
 {
     use HasFactory;
     protected $fillable = [
         'lname','fname','email','phone','company','c_no','department_id',
-        'purpose','status','checkintime','checkouttime', 'signature',
+        'purpose_id','status','checkintime','checkouttime', 'signature', 'rate',
         'comment',
     ];
 
     public function department() {
         return $this->belongsTo(Department::class);
+    }
+
+    public function purpose() {
+        return $this->belongsTo(VisitPurpose::class);
     }
 }
