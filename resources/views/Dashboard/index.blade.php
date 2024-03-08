5bc26780be2b18b86a9706d6a13706f965d32f8d
Paschal Mizengo <47353288+ngoijr3107@users.noreply.github.com>
2024-03-08 23:27:48 +0300

    add charts to dashboard

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -6,46 +6,165 @@
     </div>
 @endsection
 
+@php
+    $visitPerDepartment = App\Models\Visitor::join('departments', 'visitors.department_id', '=', 'departments.id')
+        ->select('departments.department_name as department_name', DB::raw('count(visitors.id) as visits'))
+        ->whereDate('visitors.updated_at', today())
+        ->groupBy('departments.department_name')
+        ->get();
+
+    $ratePerDepartment = App\Models\Visitor::join('departments', 'visitors.department_id', '=', 'departments.id')
+        ->select('departments.department_name as department_name', DB::raw('AVG(visitors.rate) as average_rating'))
+        ->whereNotNull('visitors.rate')
+        ->whereDate('visitors.updated_at', today())
+        ->groupBy('department_name')
+        ->get();
+
+    $underratedThreshold = 3.5; // You can adjust this threshold as needed
+
+    $underratedDepartments = $ratePerDepartment->filter(function ($item) use ($underratedThreshold) {
+        return $item->average_rating < $underratedThreshold;
+    });
+@endphp
+
 @section('content')
     <div class="row">
-        <div class="col-md-4">
-            <div class="card shadow-sm p-2">
-                <p>Total visitors</p>
-                <h5>{{ $visitors }}</h5>
-            </div>
+        <div class="col-md-12">
+            <p>Today's Overview, {{ date('M d/Y') }}</p>
         </div>
+    </div>
+
+    <div class="row">
         <div class="col-md-4">
-            <div class="card shadow-sm p-2">
-                <p>Total visitors</p>
-                <h5>{{ $visitors }}</h5>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card shadow-sm p-2">
-                <p>Total visitors</p>
-                <h5>{{ $visitors }}</h5>
-            </div>
-        </div>
-        {{-- <div class="col-sm-12">
             <div class="box">
                 <div class="box-header">
                     <div class="row">
                         <div class="col-12 text-center">
-                            <img src="{{ asset('img/under-construction.jpg') }}" class="" style="height: 400px; object-fit: cover" alt="User Image">
+                            <p>Total Visitors</p>
+                            <h5>{{ $visitors }}</h5>
                         </div>
                     </div>
                 </div>
             </div>
-        </div> --}}
+        </div>
+        <div class="col-md-4">
+            <div class="box">
+                <div class="box-header">
+                    <div class="row">
+                        <div class="col-12 text-center">
+                            <p>Visitors in</p>
+                            <h5>{{ $checkedin }}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="box">
+                <div class="box-header">
+                    <div class="row">
+                        <div class="col-12 text-center">
+                            <p>Checked-out</p>
+                            <h5>{{ $checkedout }}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="box">
+                <div class="box-header">
+                    <div id="chart"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6">
+            <div class="box">
+                <div class="box-header">
+                    <div id="rating-chart"></div>
+                    {{-- <p>Underrated Departments:</p>
+                    <ul>
+                        @foreach ($underratedDepartments as $department)
+                            <li>{{ $department->department_name }} (Average Rating: {{ $department->average_rating }})</li>
+                        @endforeach
+                    </ul> --}}
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
 @section('script')
-    <script src="{{asset('vendor/highchartjs/highcharts.js')}}"></script>
-    <script src="{{asset('vendor/highchartjs/exporting.js')}}"></script>
-    <script src="{{asset('vendor/highchartjs/export-data.js')}}"></script>
-    <script src="{{asset('vendor/highchartjs/accessibility.js')}}"></script>
+    <script>
+        var options = {
+            chart: {
+                type: 'bar',
+            },
+            title: {
+                text: 'Number of Visits per Department',
+                align: 'center',
+                fontFamily: 'Poppins'
+            },
+            stroke: {
+                curve: 'smooth'
+            },
+            series: [{
+                name: 'Visits per Department',
+                data: @json($visitPerDepartment->pluck('visits')->map('intval'))
+            }],
+            xaxis: {
+                categories: @json($visitPerDepartment->pluck('department_name'))
+            }
+        }
+
+        var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+        chart.render();
+
+        // Rating chat per department
+        var options = {
+            chart: {
+                type: 'bar',
+            },
+            title: {
+                text: 'Average Rating per Department',
+                align: 'center'
+            },
+            series: [{
+                name: 'Average Rating',
+                data: @json($ratePerDepartment->pluck('average_rating')),
+            }],
+            xaxis: {
+                categories: @json($ratePerDepartment->pluck('department_name'))
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: true,
+                        // position: 'top',
+                        formatter: function(val) {
+                            return val < 3.5 ? 'Poor' : 'Better';
+                        },
+                        offsetY: -10
+                    }
+                }
+            }
+        }
+
+
+        var ratingChart = new ApexCharts(document.querySelector("#rating-chart"), options);
+
+        ratingChart.render();
+    </script>
+
+    <script src="{{ asset('vendor/highchartjs/highcharts.js') }}"></script>
+    <script src="{{ asset('vendor/highchartjs/exporting.js') }}"></script>
+    <script src="{{ asset('vendor/highchartjs/export-data.js') }}"></script>
+    <script src="{{ asset('vendor/highchartjs/accessibility.js') }}"></script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <script src="https://code.highcharts.com/modules/export-data.js"></script>
