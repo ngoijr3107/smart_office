5c666184628058105180703d712c56a2c2f1f253
Paschal Mizengo <47353288+ngoijr3107@users.noreply.github.com>
2024-03-08 23:26:36 +0300

    add charts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,16 +7,45 @@ use App\Models\LockerLog;
 use App\Models\Attendance;
 use App\Models\Visitor;
 use Illuminate\Support\Facades\Response;
-use DB;
+// use DB;
+use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use DateTime;
 
 class DashboardController extends Controller
 {
+
     public function index()
     {
-        $visitors = Visitor::count('id');
-        return view('Dashboard.index', compact('visitors',));
+        $today = Carbon::today();
+        
+        // Count of all visitors created today
+        $visitors = Visitor::whereDate('created_at', $today)->count('id');
+
+        // Count of checked-out visitors created today with status 'out'
+        $checkedin = Visitor::where('status', 'in')->whereDate('created_at', $today)->count('id');
+    
+        // Count of checked-out visitors created today with status 'out'
+        $checkedout = Visitor::where('status', 'out')->whereDate('updated_at', $today)->count('id');
+    
+        return view('Dashboard.index', compact('visitors', 'checkedout', 'checkedin'));
     }
+
+    public function chart($startDate = null, $endDate = null)
+    {
+        $data = Visitor::join('departments', 'visitors.department_id', '=', 'departments.id')
+            ->select(DB::raw('DATE(visitors.created_at) as date'), 'departments.name as department_name', DB::raw('count(visitors.id) as visits'))
+            ->when($startDate, function ($query, $startDate) {
+                return $query->where('visitors.created_at', '>=', $startDate);
+            })
+            ->when($endDate, function ($query, $endDate) {
+                return $query->where('visitors.created_at', '<=', $endDate);
+            })
+            ->groupBy('date', 'departments.name')
+            ->get();
+    
+        return view('Dashboard.index', compact('data'));
+    }
+    
 
 }
